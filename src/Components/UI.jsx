36ed3096c5e83bbe34fca4ec32d8c14fe93c0217
Anurag7247 <anuragtiwari7047@gmail.com>
2024-01-13 15:59:16 +0530

    reactupdate speaker

--- src/Components/UI.jsx
+++ src/Components/UI.jsx
@@ -3,19 +3,22 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import InputBox from './InputBox';
 import WeatherDisplay from './WeatherDisplay';
-
+import speakerIcon from '../Images/pngwing.com.png'
 const UI = () => {
   let [cityName, updateCityName] = useState('');
   let [weatherData, updateWeatherData] = useState(null);
   const [currentTime, setCurrentTime] = useState(new Date());
+  const [spokenText, setSpokenText] = useState('');
 
   const submit = () => {
     Api();
   };
 
   const Api = () => {
-    axios.get(`http://api.weatherapi.com/v1/current.json?key=d384dff0c77c4b33963131810241001&q=${cityName}/`).then((response) => {
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=d384dff0c77c4b33963131810241001&q=${cityName}/`).then((response) => {
       const data = response.data;
+      const spokenText = `The temperature in ${cityName} is ${data.current.temp_c} degrees Celsius.`;
+      setSpokenText(spokenText);
       updateWeatherData({
         cityName: cityName,
         icons: data.current.condition.icon,
@@ -26,6 +29,16 @@ const UI = () => {
         wind: data.current.wind_kph,
       });
     });
+  };
+
+  const speakTemperature = () => {
+    if ('speechSynthesis' in window) {
+      const synthesis = window.speechSynthesis;
+      const utterance = new SpeechSynthesisUtterance(spokenText);
+      synthesis.speak(utterance);
+    } else {
+      console.error('Speech synthesis not supported');
+    }
   };
 
   const currentFormattedDate = () => {
@@ -52,17 +65,22 @@ const UI = () => {
         </div>
         <div className="row" id="outPut">
           {weatherData && (
-            <WeatherDisplay
-              cityName={weatherData.cityName}
-              icons={weatherData.icons}
-              weather={weatherData.weather}
-              temp={weatherData.temp}
-              feellike={weatherData.feellike}
-              humi={weatherData.humi}
-              wind={weatherData.wind}
-              currentFormattedDate={currentFormattedDate}
-              currentFormattedTime={currentFormattedTime}
-            />
+            <>
+              <WeatherDisplay
+                cityName={weatherData.cityName}
+                icons={weatherData.icons}
+                weather={weatherData.weather}
+                temp={weatherData.temp}
+                feellike={weatherData.feellike}
+                humi={weatherData.humi}
+                wind={weatherData.wind}
+                currentFormattedDate={currentFormattedDate}
+                currentFormattedTime={currentFormattedTime}
+              />
+              <button onClick={speakTemperature} className="speaker-button btn btn-danger" style={{width:"50px",height:"50px",borderRadius:"60px"}}>
+                <img style={{width:"35px",height:"35px", marginLeft:"-17.5px", marginTop:"-5px"}} src={speakerIcon} alt="" />
+              </button>
+            </>
           )}
         </div>
       </div>
--- src/Components/WeatherDisplay.jsx
+++ src/Components/WeatherDisplay.jsx
@@ -3,70 +3,72 @@ import './weatherDisplay.css';
 import weathericon from '../Images/weather-app.png';
 import cityicon from '../Images/cityscape.png';
 import humidity from '../Images/air.png';
-import windicon from '../Images/wind.png'
+import windicon from '../Images/wind.png';
+
 const WeatherDisplay = ({ cityName, icons, weather, temp, feellike, humi, wind, currentFormattedDate, currentFormattedTime }) => {
   return (
-    <div className="col-12 col-md-4 offset-md-4">
+    <div className="col-12 col-md-8 offset-md-2 col-lg-4 offset-lg-4">
       <div className="card border-danger mb-3">
-         <div className="card-header text-center">
-           <h2>
-             Current weather
-             <img src={weathericon} style={{ height: '40px', width: '40px' }} alt="weather icon" />
-           </h2>
-         </div>
-         <div className="card-body">
-           <div className="w-75 bg-opacity" style={{height:"60px"}}>
-             <div className="">
-               <img src={icons} alt="weather icon"/>
-               {weather} 
-             <div style={{marginLeft:"170px",marginTop:"-60px"}}>
-             <p>{currentFormattedDate()}<br/>
-             {currentFormattedTime()}</p>
-             </div>
-             </div>
-           </div>
-           
-           <br />
-           <div className=" bg-opacity text-center" style={{width:"120px"}}>
-             <h2>{temp}°C</h2>
-             <p>
-               Feels like
-               <br />
-               {feellike}°C 
-             </p>
-           </div>
-           <div className="w-100 bg-opacity text-center">
-             <div className="row">
-               
-               <div className="col-4" style={{paddingTop:"10px"}}>
-                 <p>
-                   City
-                   <img src={cityicon} style={{ height: '25px', width: '25px' }} alt="city icon" />
-                   <br />
-                   {cityName}
-                 </p>
-               </div>
-               <div className="col-4" style={{paddingTop:"10px"}}>
-                 <p>
-                   Humidity
-                   <img src={humidity} style={{ height: '25px', width: '25px' }} alt="air icon" />
-                   <br />
-                   {humi}%
-                 </p>
-               </div>
-               <div className="col-4" style={{paddingTop:"10px"}}>
-                 <p>
-                   Wind
-                   <img src={windicon} style={{ height: '25px', width: '25px' }} alt="wind icon" />
-                   <br />
-                   {wind}km/h
-                 </p>
-              
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
+        <div className="card-header text-center">
+          <h2>
+            Current weather
+            <img src={weathericon} style={{ height: '40px', width: '40px' }} alt="weather icon" />
+          </h2>
+        </div>
+        <div className="card-body">
+          <div className="bg-opacity" style={{ height: "60px", width: "90%" }}>
+            <div className="d-flex justify-content-between align-items-center">
+              <div>
+                <img src={icons} alt="weather icon" />
+                {weather}
+              </div>
+              <div>
+                <p style={{marginRight:"10px",marginTop:"5px"}} className="text-center">{currentFormattedDate()}<br />
+                  {currentFormattedTime()}</p>
+              </div>
+            </div>
+          </div>
+
+          <br />
+          <div className="bg-opacity text-center" style={{ width: "120px" }}>
+            <h2>{temp}°C</h2>
+            <p>
+              Feels like
+              <br />
+              {feellike}°C
+            </p>
+          </div>
+
+          <div className="w-100 bg-opacity text-center">
+            <div className="row">
+              <div className="col-4 col-md-4 mb-md-0">
+                <p style={{paddingTop:"12px"}}>
+                  City
+                  <img src={cityicon} style={{ height: '25px', width: '25px'}} alt="city icon" />
+                  <br />
+                  {cityName}
+                </p>
+              </div>
+              <div className="col-4 col-md-4 mb-md-0">
+                <p style={{paddingTop:"12px"}}>
+                  Humidity
+                  <img src={humidity} style={{ height: '25px', width: '25px' }} alt="air icon" />
+                  <br />
+                  {humi}%
+                </p>
+              </div>
+              <div className="col-4 col-md-4">
+                <p style={{paddingTop:"12px"}}>
+                  Wind
+                  <img src={windicon} style={{ height: '25px', width: '25px' }} alt="wind icon" />
+                  <br />
+                  {wind}km/h
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
